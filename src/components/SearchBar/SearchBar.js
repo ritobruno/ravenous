a596e7abbeff73f760f3d642140484b40b6b724a
10162543 <BrunoRito@brunomacbookpro.lan>
2017-11-21 23:23:08 +0000

    changes to search bar - part II

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,16 +4,59 @@ import './SearchBar.css';
 let sortByOptions = {
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
+  'Most Reviewed': 'review_count',
+
+  constructor(props){
+    this.handleTermChange.bind(this);
+    this.handleLocationChange.bind(this);
+  },
+
+  getSortByClass(sortByOption) {
+    if(this.state.sortBy == sortByOption){
+      return 'active';
+    }
+    else {
+      return ' ';
+    }
+  },
+
+  handleSortByChange(sortByOption){
+    this.setState(
+      {sortBy: this.state.sortBy }
+    );
+  },
+
+  handleTermChange(e){
+    this.setState({
+      term: e.target.value
+    });
+  },
+  handleLocationChange(e){
+    this.setState({
+      location: e.target.value
+    });
+  }
 };
 
+
+
 class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      term:' ',
+      location:' ' ,
+      sortBy: 'best_match'
+    };
+  }
+
   renderSortByOptions(){
     return Object.keys(sortByOptions).map(i => {
       let sortByOptionValue = sortByOptions[i];
-      return <li key='{sortByOptionValue}'>{i}</li>;
+      return <li className='{getSortByClass(sortByOptionValue)}' key='{sortByOptionValue}' onClick='{handleSortByChange.bind(this, sortByOptionValue)}' >{i}</li>;
     });
   };
+
   render(){
     return (
       <div className="SearchBar">
@@ -23,8 +66,8 @@ class SearchBar extends React.Component{
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange='{this.handleTermChange}' placeholder="Search Businesses" />
+          <input onChange='{this.handleLocationChange}' placeholder="Where?" />
         </div>
         <div className="SearchBar-submit">
           <a>Lets Go</a>
