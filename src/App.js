cd1133edb822b2204b90de92e7a856eaa00da2bc
Bruno <ritobruno@gmail.com>
2017-11-26 17:52:35 +0000

    More fixes to Yelp.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import BusinessList from './components/BusinessList/BusinessList.js';
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,40 +1,41 @@
-import React from 'react';
 
 const clientId = '2Ecw4HUVMYgeNN5Yw1-O4Q';
 const secret = 'EtljJNGcmA9ZmhzkhpLlXi1eymjxMrhdmpagD6kIKgc8JGmxJhiJua3RwiXFgsZh';
 
-let accessToken = '';
+let accessToken ='';
 
-let Yelp = {
+const Yelp = {
   getAccessToken(){
 
     if(accessToken){
       return new Promise(resolve => resolve(accessToken));
     }
-    return fetch( `https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=` + clientId + `&client_secret=` + secret, {
+    return fetch( `https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
       method: 'POST'
     }).then(response => {
       response.json()
-    }).then(jsonResponse => {accessToken = jsonResponse.access_token});
+    }).then(jsonResponse => accessToken = jsonResponse.access_token);
   },
   search(term, location, sortBy){
     return Yelp.getAccessToken().then(() => {
-      return fetch('https://api.yelp.com/v3/businesses/search?term=' + term + '&location=' + location + '&sort_by=' + sortBy, {
+      return fetch('https://api.yelp.com/v3/businesses/search?term=' + term + `&location=` + location + `&sort_by=` + sortBy, {
         headers: {Authorization: `Bearer ${accessToken}`}
       } )
-    }).then(response => {return response.json()}).then(jsonResponse => {
+    }).then(response => response.json()).then(jsonResponse => {
       if(jsonResponse.businesses){
         return jsonResponse.businesses.map( business => ({
-          id: business.id,
-          imageSrc: business.image_url,
-          name: business.name,
-          adress: business.adress,
-          city: business.city,
-          state: business.state,
-          zipCode: business.zipCode,
-          category: business.category,
-          rating: business.rating,
-          reviewCount: business.reviewCount
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            adress: business.adress,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+
+
         }));
       }
     });
